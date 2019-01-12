540a68b61be78cc6a2a048eda4d6a4a056bc56ea
Drew Tyre <atyre2@unl.edu>
2019-01-12 11:20:44 -0600

    add roxygen skeleton

--- R/themes.R
+++ R/themes.R
@@ -1,3 +1,12 @@
+#' Default NRES theme
+#'
+#' @param base_size
+#' @param base_family
+#'
+#' @return a ggplot2 theme object
+#' @export
+#'
+#' @examples
 theme_nres <- function(base_size = 16, base_family = "sans"){
   theme_bw() + theme(
     plot.caption = element_text(hjust = 0)
