699ec37c24a632a759178a05b33cb5a46fa261eb
Drew Tyre <atyre2@unl.edu>
2019-01-12 15:46:14 -0600

    add ggplot2 to imports

--- R/themes.R
+++ R/themes.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 theme_nres <- function(base_size = 16, base_family = "sans"){
-  theme_bw() + theme(
-    plot.caption = element_text(hjust = 0)
+  ggplot2::theme_bw() + ggplot2::theme(
+    plot.caption = ggplot2::element_text(hjust = 0)
   )
 }
